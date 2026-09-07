7adad6623e1076b3beb97193234f9ff230d829b2
Mark Knapp <makman99@gmail.com>
2026-09-07 15:20:36 -0400

    TryParse WeekDay

--- Ical.Net/Serialization/DataTypes/WeekDaySerializer.cs
+++ Ical.Net/Serialization/DataTypes/WeekDaySerializer.cs
@@ -1,12 +1,10 @@
-﻿//
+//
 // Copyright ical.net project maintainers and contributors.
 // Licensed under the MIT license.
 //
 
 using System;
-using System.Globalization;
 using System.IO;
-using System.Text.RegularExpressions;
 using Ical.Net.DataTypes;
 
 namespace Ical.Net.Serialization.DataTypes;
@@ -47,35 +45,15 @@ public class WeekDaySerializer : EncodableDataTypeSerializer
         return Encode(ds, value);
     }
 
-    private static readonly Regex _dayOfWeek = new Regex(@"(\+|-)?(\d{1,2})?(\w{2})", RegexOptions.Compiled | RegexOptions.IgnoreCase, RegexDefaults.Timeout);
-
     public override object? Deserialize(TextReader tr)
     {
         var value = tr.ReadToEnd();
 
-        // Create the day specifier and associate it with a calendar object
-        if (CreateAndAssociate() is not WeekDay ds)
-            return null;
-
-        // Decode the value, if necessary
-        value = Decode(ds, value);
-        if (value == null) return null;
-
-        var match = _dayOfWeek.Match(value);
-        if (!match.Success)
+        if (WeekDay.TryParse(value, out var weekDay))
         {
-            return null;
+            return weekDay;
         }
 
-        if (match.Groups[2].Success)
-        {
-            ds.Offset = Convert.ToInt32(match.Groups[2].Value, CultureInfo.InvariantCulture);
-            if (match.Groups[1].Success && match.Groups[1].Value.Contains("-"))
-            {
-                ds.Offset *= -1;
-            }
-        }
-        ds.DayOfWeek = RecurrenceRuleSerializer.GetDayOfWeek(match.Groups[3].Value);
-        return ds;
+        return null;
     }
 }
