9d1e8bccf9fd9409a1ee286c3dbe0d13fc3592ad
Mark Knapp <makman99@gmail.com>
2026-09-07 09:17:29 -0400

    Reduce direct use of PeriodListSerializer

--- Ical.Net/Serialization/DataTypes/PeriodListSerializer.cs
+++ Ical.Net/Serialization/DataTypes/PeriodListSerializer.cs
@@ -1,12 +1,10 @@
-﻿//
+//
 // Copyright ical.net project maintainers and contributors.
 // Licensed under the MIT license.
 //
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Ical.Net.DataTypes;
 
 namespace Ical.Net.Serialization.DataTypes;
@@ -21,83 +19,33 @@ public class PeriodListSerializer : EncodableDataTypeSerializer
 
     public override string? SerializeToString(object? obj)
     {
-        var factory = GetService<ISerializerFactory>();
         if (obj is not PeriodList periodList)
         {
             return null;
         }
 
-        var dtSerializer = factory.Build(typeof(CalDateTime), SerializationContext) as IStringSerializer;
-        var periodSerializer = factory.Build(typeof(Period), SerializationContext) as IStringSerializer;
-        if (dtSerializer == null || periodSerializer == null)
-        {
-            return null;
-        }
-
-        var parts = new List<string?>(periodList.Count);
-
-        var firstPeriod = periodList.FirstOrDefault();
-
-        // Set TzId before ValueType, so that it serializes first
-        if (firstPeriod != null && !string.IsNullOrEmpty(firstPeriod.TzId) && firstPeriod.TzId != "UTC")
-        {
-            periodList.Parameters.Set("TZID", periodList[0].TzId);
-        }
-
-        switch (firstPeriod?.PeriodKind) // default type is DATE-TIME
-        {
-            case PeriodKind.Period:
-                periodList.SetValueType("PERIOD");
-                break;
-            case PeriodKind.DateOnly:
-                periodList.SetValueType("DATE");
-                break;
-        }
-
-        foreach (var p in periodList)
-        {
-            parts.Add(p.HasEndOrDuration
-                ? periodSerializer.SerializeToString(p)
-                : dtSerializer.SerializeToString(p.StartTime));
-        }
-
-        return Encode(periodList, string.Join(",", parts));
+        return periodList.ToString();
     }
 
     public override object? Deserialize(TextReader tr)
     {
         var value = tr.ReadToEnd();
 
-        // Create the day specifier and associate it with a calendar object
-        if (CreateAndAssociate() is not PeriodList rdt) return null;
-
-        var factory = GetService<ISerializerFactory>();
-
-        // Decode the value, if necessary
-        value = Decode(rdt, value);
-
-        var dtSerializer = factory.Build(typeof(CalDateTime), SerializationContext) as IStringSerializer;
-        var periodSerializer = factory.Build(typeof(Period), SerializationContext) as IStringSerializer;
-        if (value == null || dtSerializer == null || periodSerializer == null)
-        {
-            return null;
-        }
+        PeriodList periodList = [];
 
         var values = value.Split(',');
         foreach (var v in values)
         {
-            var dt = dtSerializer.Deserialize(new StringReader(v)) as CalDateTime;
-            var p = periodSerializer.Deserialize(new StringReader(v)) as Period;
-
-            if (dt != null)
+            if (CalDateTime.TryParse(v, out var duration))
             {
-                rdt.Add(dt);
+                periodList.Add(duration);
             }
-            else if (p != null)
+            else if (Period.TryParse(v, null, out var period))
             {
-                rdt.Add(p);
+                periodList.Add(period);
             }
         }
-        return rdt;
+
+        return periodList;
     }
 }
